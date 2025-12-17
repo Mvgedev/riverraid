e5a046d66ec11c79f77b6033c363669bb46f50bc
Mvge <mvge.dev@gmail.com>
2025-12-17 11:00:39 +0100

    Added Main Menu and Settings transition
    - Added Slider Texture and Style
    - Added Buttons group on main menu
    - Added AnimationPlayer
    - Added Animation transition between Main Menu <> Settings

---
+++ scripts/gamescenes/main_menu.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_settings_button_pressed() -> void:
+	animation_player.play("To_Settings")
+
+# Settings Page
+
+func _on_back_button_pressed() -> void:
+	animation_player.play_backwards("To_Settings")
